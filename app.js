d4cf22e8837c9be67901566592e2ea2351b75735
hamidabdulov <hemidabdulov@gmail.com>
2024-05-19 12:51:13 -0700

    UPDATE

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ fetch('https://api.fastforex.io/fetch-all?api_key=d0397fe669-3b73bc8f9d-sdfc74',
   .then(data => {
     const rates = data.results;
     exchangeRates = {
-      "EUR_USD": rates.USD / rates.EUR,
+      "EUR_USD": rates.EUR / rates.USD,
       "USD_EUR": rates.USD / rates.EUR,
       "EUR_RUB": rates.EUR / rates.RUB,
       "RUB_EUR": rates.RUB / rates.EUR,
-      "EUR_GBP": rates.GBP / rates.EUR,
-      "GBP_EUR": rates.EUR / rates.GBP,
+      "EUR_GBP": rates.EUR / rates.GBP,
+      "GBP_EUR": rates.GBP / rates.EUR,
       "USD_RUB": rates.USD / rates.RUB,
       "RUB_USD": rates.RUB / rates.USD,
       "USD_GBP": rates.USD / rates.GBP,
       "GBP_USD": rates.GBP / rates.USD,
       "RUB_GBP": rates.RUB / rates.GBP,
       "GBP_RUB": rates.GBP / rates.RUB,
-      "RUB_RUB": rates.RUB / rates.RUB,
-      "USD_USD": rates.USD / rates.USD,
-      "EUR_EUR": rates.EUR / rates.EUR,
-      "GBP_GBP": rates.GBP / rates.GBP,
+      "RUB_RUB": 1,
+      "USD_USD": 1,
+      "EUR_EUR": 1,
+      "GBP_GBP": 1
     };
     updateButtonStyles();
     updateSelectedCurrencies();
@@ -58,15 +58,15 @@ function updateSelectedCurrencies() {
   let rate2 = exchangeRates[currency2 + "_" + currency1];
   if (rate1 && rate2) {
     document.getElementById('selected-currencies').innerText = `1 ${currency1} = ${rate1.toFixed(4)} ${currency2}`;
-    document.getElementById('selected-currencies-duplicate').innerText = `1 ${currency2} = ${rate2.toFixed(4)} ${currency1}`;
+    document.getElementById('selected-currencies-duplicate').innerText = `1 ${currency1} = ${rate2.toFixed(4)} ${currency2}`;
   } else {
     document.getElementById('selected-currencies').innerText = `Курс для ${currency1} и ${currency2} недоступен`;
-    document.getElementById('selected-currencies-duplicate').innerText = `Курс для ${currency2} и ${currency1} недоступен`;
+    document.getElementById('selected-currencies-duplicate').innerText = `Курс для ${currency1} и ${currency2} недоступен`;
   }
 }
 
 function convert() {
-  let amount1 = parseFloat(document.getElementById('amount1').value);
+  let amount1 = parseFloat(document.getElementById('amount1').value.replace(/,/g, '.'));
   if (isNaN(amount1) || !exchangeRates[currency1 + "_" + currency2]) {
     document.getElementById('amount2').value = '';
     return;
@@ -90,8 +90,8 @@ function replaceCommaWithPeriod(event) {
 document.addEventListener('DOMContentLoaded', () => {
   updateButtonStyles();
   document.getElementById('amount1').addEventListener('input', convert);
-  document.getElementById('amount1').addEventListener('keydown', preventInvalidInput);
   document.getElementById('amount1').addEventListener('input', replaceCommaWithPeriod);
+  document.getElementById('amount1').addEventListener('keydown', preventInvalidInput);
   document.getElementById('amount2').addEventListener('keydown', preventInvalidInput);
   document.getElementById('amount2').addEventListener('input', replaceCommaWithPeriod);
 });
