449395b8cd6ab9bb5830d8056d197d5c0e89d0d9
hamidabdulov <hemidabdulov@gmail.com>
2024-05-19 12:34:20 -0700

    update

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ fetch('https://api.fastforex.io/fetch-all?api_key=d0397fe669-3b73bc8f9d-sdfc74',
     const rates = data.results;
     exchangeRates = {
       "EUR_USD": rates.USD / rates.EUR,
-      "USD_EUR": rates.EUR / rates.USD,
-      "EUR_RUB": rates.RUB / rates.EUR,
-      "RUB_EUR": rates.EUR / rates.RUB,
+      "USD_EUR": rates.USD / rates.EUR,
+      "EUR_RUB": rates.EUR / rates.RUB,
+      "RUB_EUR": rates.RUB / rates.EUR,
       "EUR_GBP": rates.GBP / rates.EUR,
       "GBP_EUR": rates.EUR / rates.GBP,
-      "USD_RUB": rates.RUB / rates.USD,
-      "RUB_USD": rates.USD / rates.RUB,
-      "USD_GBP": rates.GBP / rates.USD,
-      "GBP_USD": rates.USD / rates.GBP,
-      "RUB_GBP": rates.GBP / rates.RUB,
-      "GBP_RUB": rates.RUB / rates.GBP,
-	    "RUB_RUB": rates.RUB / rates.RUB,
+      "USD_RUB": rates.USD / rates.RUB,
+      "RUB_USD": rates.RUB / rates.USD,
+      "USD_GBP": rates.USD / rates.GBP,
+      "GBP_USD": rates.GBP / rates.USD,
+      "RUB_GBP": rates.RUB / rates.GBP,
+      "GBP_RUB": rates.GBP / rates.RUB,
+      "RUB_RUB": rates.RUB / rates.RUB,
       "USD_USD": rates.USD / rates.USD,
       "EUR_EUR": rates.EUR / rates.EUR,
       "GBP_GBP": rates.GBP / rates.GBP,
@@ -77,7 +77,21 @@ function convert() {
   document.getElementById('amount2').value = convertedAmount.toFixed(4);
 }
 
+function preventInvalidInput(event) {
+  if (['e', 'E', '+', '-'].includes(event.key)) {
+    event.preventDefault();
+  }
+}
+
+function replaceCommaWithPeriod(event) {
+  event.target.value = event.target.value.replace(/,/g, '.');
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   updateButtonStyles();
   document.getElementById('amount1').addEventListener('input', convert);
+  document.getElementById('amount1').addEventListener('keydown', preventInvalidInput);
+  document.getElementById('amount1').addEventListener('input', replaceCommaWithPeriod);
+  document.getElementById('amount2').addEventListener('keydown', preventInvalidInput);
+  document.getElementById('amount2').addEventListener('input', replaceCommaWithPeriod);
 });
